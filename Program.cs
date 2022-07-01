9f4d88435a8df7b172216b946d90df47228db3d8
Emily1028 <81636593+Emily1028@users.noreply.github.com>
2022-07-01 20:56:40 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B0226497_段昱如_PDPR_week5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("B0226497_段昱如_PDPR_week5_2");
+            Console.WriteLine("故人具雞黍，邀我至田家。");
+            Console.WriteLine(" 綠樹村邊合，青山郭外斜。");
+            Console.WriteLine("   開軒面場圃，把酒話桑麻。");
+            Console.WriteLine("     待到重陽日，還來就菊花。");
+            Console.ReadLine();
+        }
+    }
+}
